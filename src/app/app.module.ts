2f447e51b1a4d8a21ea8914adc6da8a59c84d418
Ameya Kasar <akasar@esi-estech.com>
2018-08-01 11:05:34 -0500

    Adding product list screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {FormsModule} from '@angular/forms'
 
 import { AppComponent } from './app.component';
+import { ListOfProductsComponent } from './products/product-list.component';
+import { ReplaceToSpaces } from './shared/replace-tospaces.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListOfProductsComponent,
+    ReplaceToSpaces
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
