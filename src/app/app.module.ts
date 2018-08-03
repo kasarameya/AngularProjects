3645adb8bbeed6b7535f78ec13f7e47d58b2c8b9
Ameya Kasar <akasar@esi-estech.com>
2018-08-03 17:16:47 -0500

    Adding product detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,16 @@ import { AppComponent } from './app.component';
 import { ListOfProductsComponent } from './products/product-list.component';
 import { ReplaceToSpaces } from './shared/replace-tospaces.pipe';
 import { StarRating } from './shared/star.component';
-import {HttpClientModule} from '@angular/common/http'
+import {HttpClientModule} from '@angular/common/http';
+import { ProductDetailComponent } from './products/product-detail/product-detail.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     ListOfProductsComponent,
     ReplaceToSpaces,
-    StarRating
+    StarRating,
+    ProductDetailComponent
   ],
   imports: [
     BrowserModule,
