5e0fb8c16352d0dc0dfe57366d0f02ec6ee6c203
Ameya Kasar <akasar@esi-estech.com>
2018-08-13 10:27:53 -0500

    Routing completed with back button

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '../../../node_modules/@angular/router';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,9 +10,27 @@ export class ProductDetailComponent implements OnInit {
   prodTitle : string;
   productItem : IProduct;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router ) { }
 
   ngOnInit() {
+    let prodId = +this.route.snapshot.paramMap.get('id');
+    console.log("inside init of details",prodId);
+    this.prodTitle += ` : ${prodId}`;
+    this.productItem = {
+      "productId": prodId,
+      "productName": "Leaf Rake",
+      "productCode": "GDN-0011",
+      "releaseDate": "March 19, 2016",
+      "description": "Leaf rake with 48-inch wooden handle.",
+      "price": 19.95,
+      "starRating": 3.2,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
+    }
+  }
+
+  onBackPressed():void{
+    this.router.navigate(['/productList'])
   }
 
 }
